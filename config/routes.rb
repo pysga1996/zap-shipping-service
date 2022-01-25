b177e82fe1364ffee9a17f5030c1f13fc55eb2e5
thanhvt <thanhvt@vissoft.vn>
2022-01-26 00:07:36 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/api/shipment', to: 'shipment#index'
+
+  post '/api/test', to: 'shipment#read_data'
   # Almost every application defines a route for the root path ("/") at the top of this file.
   root "home#index"
 end
